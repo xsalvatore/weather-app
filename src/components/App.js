d9231f9834680c3338c283c3d253f0a57e640fce
Xavier Salvatore <xavier.salvatore@sogematech.com>
2019-08-22 12:05:51 -0400

    Fixed formatting, added API keys

--- src/components/App.js
+++ src/components/App.js
@@ -3,125 +3,173 @@ import axios from 'axios';
 import key from '../config/keys.js';
 
 import Form from './Form.js';
-import Data from './Data.js'
+import Data from './Data.js';
 
 class App extends Component {
 	state = {
-		text: "",
+		text: '',
 		currentData: [],
 		dailyData: [],
 		weeklyData: [],
-		loaded: false,
-	}
+		loaded: false
+	};
 
-	handleInputOnChange = e => {
+	handleInputOnChange = (e) => {
 		this.setState({
 			text: e.target.value
 		});
-	}
+	};
 
-	handleFormOnSubmit = e => {
+	handleFormOnSubmit = (e) => {
 		e.preventDefault();
 
 		this.handleWeatherFetch();
-	}
-	
-	handleWeatherFetch = async (e) => {
-		axios.all([
-			axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.text}&appid=${key.openweathermap}&units=metric`),
-			axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.text}&appid=${key.openweathermap}&units=metric&cnt=5`),
-			axios.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${this.state.text}&appid=${key.openweathermap}&units=metric&cnt=6`)
-		])
-		.then(axios.spread((currentResponse, dailyResponse, weeklyResponse) => {
-			/* ================================================== */
-			
-			let currentData = {};
+	};
 
-			const date = new Date();
-			const hour = date.getHours();
+	handleWeatherFetch = async (e) => {
+		axios
+			.all([
+				axios.get(
+					`https://api.openweathermap.org/data/2.5/weather?q=${
+						this.state.text
+					}&appid=${key.openweathermap}&units=metric`
+				),
+				axios.get(
+					`http://api.openweathermap.org/data/2.5/forecast?q=${
+						this.state.text
+					}&appid=${key.openweathermap}&units=metric&cnt=5`
+				),
+				axios.get(
+					`http://api.openweathermap.org/data/2.5/forecast/daily?q=${
+						this.state.text
+					}&appid=${key.openweathermap}&units=metric&cnt=6`
+				)
+			])
+			.then(
+				axios.spread(
+					(currentResponse, dailyResponse, weeklyResponse) => {
+						/* ================================================== */
 
-			currentData.hour = hour;
-			currentData.temperature = Math.round(currentResponse.data.main.temp);
+						let currentData = {};
 
-			if(currentData.hour >= 21 || currentData.hour <= 3)
-				currentData.icon = `night/${currentResponse.data.weather[0].main.toLowerCase()}`;	
+						const date = new Date();
+						const hour = date.getHours();
 
-			else
-				currentData.icon = `day/${currentResponse.data.weather[0].main.toLowerCase()}`;
+						currentData.hour = hour;
+						currentData.temperature = Math.round(
+							currentResponse.data.main.temp
+						);
 
-			/* ================================================== */
+						if (currentData.hour >= 21 || currentData.hour <= 3)
+							currentData.icon = `night/${currentResponse.data.weather[0].main.toLowerCase()}`;
+						else
+							currentData.icon = `day/${currentResponse.data.weather[0].main.toLowerCase()}`;
 
-			let dailyData = [];
+						/* ================================================== */
 
-			for(let i=0; i<dailyResponse.data.list.length; i++) {
-				let object = {};
-				
-				let hour = dailyResponse.data.list[i].dt_txt.split(" ")[1];
-				hour = hour.substring(0, 2);
+						let dailyData = [];
 
-				object.time = hour;
+						for (
+							let i = 0;
+							i < dailyResponse.data.list.length;
+							i++
+						) {
+							let object = {};
 
-				if(object.time >= 21 || object.time <= 3)
-					object.icon = `night/${dailyResponse.data.list[i].weather[0].main.toLowerCase()}`;
+							let hour = dailyResponse.data.list[i].dt_txt.split(
+								' '
+							)[1];
+							hour = hour.substring(0, 2);
 
-				else
-					object.icon = `day/${dailyResponse.data.list[i].weather[0].main.toLowerCase()}`;
+							object.time = hour;
 
-				object.temperature = Math.round(dailyResponse.data.list[i].main.temp);
+							if (object.time >= 21 || object.time <= 3)
+								object.icon = `night/${dailyResponse.data.list[
+									i
+								].weather[0].main.toLowerCase()}`;
+							else
+								object.icon = `day/${dailyResponse.data.list[
+									i
+								].weather[0].main.toLowerCase()}`;
 
-				dailyData.push(object);
-			}
+							object.temperature = Math.round(
+								dailyResponse.data.list[i].main.temp
+							);
 
-			/* ================================================== */
+							dailyData.push(object);
+						}
 
-			let weeklyData = [];
+						/* ================================================== */
 
-			const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-			const now = date.getDay();
+						let weeklyData = [];
 
-			let index = now + 1;
+						const days = [
+							'Sun',
+							'Mon',
+							'Tue',
+							'Wed',
+							'Thu',
+							'Fri',
+							'Sat'
+						];
+						const now = date.getDay();
 
-			for(let i=0; i<5; i++) {
-				let object = {};
+						let index = now + 1;
 
-				if(index > 6)
-					index = 0;
+						for (let i = 0; i < 5; i++) {
+							let object = {};
 
-				object.day = days[index];
-				object.temperature = Math.round(weeklyResponse.data.list[i+1].temp.day);
-				object.icon = `day/${weeklyResponse.data.list[i+1].weather[0].main.toLowerCase()}`;
+							if (index > 6) index = 0;
 
-				weeklyData.push(object);
-						
-				index ++;
-			}
+							object.day = days[index];
+							object.temperature = Math.round(
+								weeklyResponse.data.list[i + 1].temp.day
+							);
+							object.icon = `day/${weeklyResponse.data.list[
+								i + 1
+							].weather[0].main.toLowerCase()}`;
 
-			/* ================================================== */
+							weeklyData.push(object);
 
-			this.setState({
-				currentData: currentData,
-				dailyData: dailyData,
-				weeklyData: weeklyData,
-				loaded: true,
-			})
-		}));
-	}
+							index++;
+						}
 
-	
+						/* ================================================== */
+
+						this.setState({
+							currentData: currentData,
+							dailyData: dailyData,
+							weeklyData: weeklyData,
+							loaded: true
+						});
+					}
+				)
+			);
+	};
 
 	render() {
 		return (
-			<div className="app">
-				<Form onSubmit={this.handleFormOnSubmit} value={this.state.text} onChange={this.handleInputOnChange} />
-				{ this.state.loaded 
-					? <Data loaded={this.state.loaded} currentData={this.state.currentData} dailyData={this.state.dailyData} weeklyData={this.state.weeklyData} />
-					: ""
-				}
+			<div className='app'>
+				<Form
+					onSubmit={this.handleFormOnSubmit}
+					value={this.state.text}
+					onChange={this.handleInputOnChange}
+				/>
+				{this.state.loaded ? (
+					<Data
+						loaded={this.state.loaded}
+						currentData={this.state.currentData}
+						dailyData={this.state.dailyData}
+						weeklyData={this.state.weeklyData}
+					/>
+				) : (
+					''
+				)}
 
 				<footer className={this.state.loaded ? `visible` : `hidden`}>
-					<div className="daily-view circular-nav active"></div>
-					<div className="weekly-view circular-nav"></div>
-				</footer>	
+					<div className='daily-view circular-nav active' />
+					<div className='weekly-view circular-nav' />
+				</footer>
 			</div>
 		);
 	}
---
+++ src/config/keys.js
@@ -0,0 +1,5 @@
+let key = {
+	openweathermap: '9391bf49186ae93f3fd6b6c2846b4338'
+};
+
+export default key;
